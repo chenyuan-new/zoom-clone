5a2e88aaf9a0c3704fdcc659e586e2d31949f02a
chen yuan <cy911110@qq.com>
2024-04-04 08:55:24 +0800

    fix: can not login

--- middleware.ts
+++ middleware.ts
@@ -1,19 +1,14 @@
 import { authMiddleware } from "@clerk/nextjs";
-const protectedRoutes = [
-  // "/",
-  "/upcoming",
-  "/previous",
-  "/recordings",
-  "/personal-room",
-  "/meeting(.*)",
-];
+// const protectedRoutes = [
+//   "/",
+//   "/upcoming",
+//   "/previous",
+//   "/recordings",
+//   "/personal-room",
+//   "/meeting(.*)",
+// ];
 export default authMiddleware({
-  publicRoutes(req) {
-    return !protectedRoutes.some((route) =>
-      new RegExp(route).test(req.nextUrl.pathname)
-    );
-  },
-  ignoredRoutes:['/icons/(.*)','/images/(.*)']
+  ignoredRoutes: ["/icons/(.*)", "/images/(.*)","/favicon.ico"],
 });
 
 export const config = {
